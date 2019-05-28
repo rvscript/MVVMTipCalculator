60c19bf0a5e01eb46582b3faeb4f1dd831661a93
Ricardo Viera <Ricardo.Viera@nycha.nyc.gov>
2019-05-28 16:16:16 -0400

    master rv- initial setup for tip calculator

---
+++ app/src/main/java/com/example/mvvmtipcalculator/model/TipCalculation.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmtipcalculator.model
+
+data class TipCalculation(
+    val checkAmount: Double = 0.0,
+    val tipPct: Int = 0,
+    val tipAmount: Double = 0.0,
+    val grandTotal: Double = 0.0
+) {
+
+}
